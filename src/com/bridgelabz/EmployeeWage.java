652527d4dd51a186d096696c5ffff531ce6ab62f
shraddhaghadge86 <shraddhaghadge86@gmail.com>
2022-11-19 19:58:52 +0530

    Emplyee Check added

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -1,4 +1,17 @@
 package com.bridgelabz;
 
 public class EmployeeWage {
+    public static void main(String[] args){
+
+        double employeeCheck = (int) Math.floor(Math.random() * 10) % 2;
+
+        System.out.println(employeeCheck);
+
+        if(employeeCheck == 1){
+            System.out.println("Employee is present");
+        }else{
+            System.out.println("Employee is absent");
+        }
+    }
+
 }
