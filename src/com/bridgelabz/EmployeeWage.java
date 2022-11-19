3eb7748bbd90f704b2dda189c0f0e551f6e126f6
shraddhaghadge86 <shraddhaghadge86@gmail.com>
2022-11-19 20:08:58 +0530

    daily employee wage added

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -1,17 +1,24 @@
 package com.bridgelabz;
 
 public class EmployeeWage {
+    static final int WAGE_PER_HOUR = 20;
+
+    static final int FULL_DAY_HOUR = 8;
+
     public static void main(String[] args){
 
         double employeeCheck = (int) Math.floor(Math.random() * 10) % 2;
-
         System.out.println(employeeCheck);
 
+        int dailyWage = 0;
         if(employeeCheck == 1){
+            dailyWage = FULL_DAY_HOUR * WAGE_PER_HOUR;
             System.out.println("Employee is present");
         }else{
             System.out.println("Employee is absent");
         }
+
+        System.out.println("Employee Wage: " + dailyWage);
     }
 
 }
