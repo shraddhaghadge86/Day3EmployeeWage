30ea898ce5461943bf7c7f8f052a3b5f439e777c
shraddhaghadge86 <shraddhaghadge86@gmail.com>
2022-11-19 21:49:52 +0530

    employee wage for 20 days

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -3,27 +3,40 @@ package com.bridgelabz;
 public class EmployeeWage {
     static final int WAGE_PER_HOUR = 20;
 
-    static final int PART_TIME_HOUR = 4;
+    static final int WORKING_DAY_PER_MONTH = 20;
 
     static final int FULL_DAY_HOUR = 8;
 
     public static void main(String[] args){
 
-        double employeeCheck = (int) Math.floor(Math.random() * 10) % 3;
-        System.out.println(employeeCheck);
+     //   double employeeCheck = (int) Math.floor(Math.random() * 10) % 3;
+     //   System.out.println(employeeCheck);
 
         int dailyWage = 0;
-        if(employeeCheck == 2){
-            dailyWage = FULL_DAY_HOUR * WAGE_PER_HOUR;
-            System.out.println("Full day: ");
-        }else if(employeeCheck == 1){
-            dailyWage = PART_TIME_HOUR * WAGE_PER_HOUR;
-            System.out.println("Part Time: ");
-        }else{
-            System.out.println("Employee is absent");
-        }
+        int dayCount = 1;
+        int totalWage = 0;
 
-        System.out.println("Employee Wage: " + dailyWage);
+        while(dayCount <= WORKING_DAY_PER_MONTH){
+            double employeeCheck = (int) Math.floor(Math.random() * 10) % 2;
+            System.out.println("\n" + employeeCheck);
+
+            switch ((int) employeeCheck){
+
+                case 1:
+                    System.out.println("Employee is present");
+                    dailyWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
+                    break;
+
+                default:
+                    System.out.println("Employee is absent");
+                    dailyWage = 0;
+            }
+
+            dayCount++;
+            totalWage = totalWage + dailyWage;
+            System.out.println("Employee Wage till day "+ (dayCount-1)+ " is: " + totalWage);
+        }
+        System.out.println("\notal Employee Wage: " + totalWage);
     }
 
 }
